cce22571d780a1d6c03d6174b4996a1bf9a22c17
Mario Munteanu <mario.munteanu@cirvianum.cat>
2024-04-24 16:41:00 +0200

    Refactor ex3.js to update list items text content

--- src/ex3/ex3.js
+++ src/ex3/ex3.js
@@ -4,3 +4,10 @@ ulElements.forEach(ul => {
     ul.querySelector('li:first-child').innerText = 'primer';
     ul.querySelector('li:last-child').innerText = 'últim';
 });
+
+// També es pot fer així:
+// const firstList = document.querySelectorAll('li:first-child');
+// const lastList = document.querySelectorAll('li:last-child');
+
+// firstList.forEach((li) => (li.textContent = 'primer'));
+// lastList.forEach((li) => (li.textContent = 'últim'));
